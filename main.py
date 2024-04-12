e7aafac8a205e3bd02e8005ff4e9562fd29dc600
Tushar Choudhary <tusharc.cse@gmail.com>
2024-04-12 22:01:36 +0530

    ADD: Get holdings and positions added

--- main.py
+++ main.py
@@ -1,52 +1,42 @@
+from typing import Union
+
+from fastapi import FastAPI, Header
 from dhanhq import dhanhq
-from dhanhq import marketfeed
+import httpx
+
+dhanUrl = "https://api.dhan.co"
 
 client_id = "client_id"
 access_token = "access_token"
 
 dhan = dhanhq(client_id, access_token)
 
-# Order place buy
-dhan.place_order(security_id='1333', #hdfcBank
-                exchange_segment=dhan.NSE,
-                transaction_type=dhan.BUY,
-                quantity=1,
-                order_type=dhan.MARKET,
-                product_type=dhan.INTRA,
-                price=0)
+app = FastAPI()
 
-#Order place sell
-dhan.place_order(security_id='1333', #hdfcBank
-                exchange_segment=dhan.NSE,
-                transaction_type=dhan.SELL,
-                quantity=1,
-                order_type=dhan.MARKET,
-                product_type=dhan.INTRA,
-                price=0)
+@app.get("/")
+def read_root():
+    return { "Hello Trader!" }
 
-# Get all orders
-dhan.get_order_list()
+@app.get("/myHoldings")
+async def get_my_holdings():
+    return dhan.get_holdings()
 
-# Get holdings
-dhan.get_holdings()
+@app.get("/myPositions")
+async def get_my_positions():
+    return dhan.get_positions()
 
-instruments = [(1, "1333"), (0, "11536")]
+@app.get("/holdings")
+async def get_holdings(access_token: str):
+    if access_token:
+        headers = {'Accept': 'application/json', 'access-token' : access_token}
+        url = f"{dhanUrl}/holdings"
+        fetcehed_holdings = httpx.get(url, headers=headers)
+        return fetcehed_holdings.json()
 
-subscription_code = marketfeed.Ticker
-
-async def on_connect(instance):
-    print("Connected to the server")
-
-async def on_message(isntance, message):
-    print("Message: ", message)
-
-print("Subscription code :", subscription_code)
-
-feed = marketfeed.DhanFeed(client_id,
-                            access_token,
-                            instruments,
-                            subscription_code,
-                            on_connect=on_connect,
-                            on_message=on_message)
-
-feed.run_forever()
+@app.get("/positions")
+async def get_positions(access_token: str):
+    if access_token:
+        headers = {'Accept': 'application/json', 'access-token' : access_token}
+        url = f"{dhanUrl}/positions"
+        fetcehed_positions = httpx.get(url, headers=headers)
+        return fetcehed_positions.json()
